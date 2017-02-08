fa198495c4e0a6169ebc157cb6c4abe9e1abd82a
Vince Cellini <vincecellini@Vinces-MacBook-Pro.local>
2017-02-08 09:20:44 -0500

    inital commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,35 @@
-console.log('wat')
+import * as UTILS from './utils.js'
+import $ from 'jquery';
+
+
+function controllerRouter(){
+  var currentRoute = window.location.hash.slice(1)
+ console.log(currentRoute)
+	if(currentRoute === 'concerts'){
+		$.getJSON('http://apis.is/concerts').then(function(serverRes){
+        console.log(serverRes)
+
+		})
+		return
+	}
+
+	if(currentRoute === 'carpools'){
+		$.getJSON('http://apis.is/rides/samferda-drivers/').then(function(serverRes){
+        console.log(serverRes)
+
+		})
+		return
+	}
+
+  if(currentRoute === 'flights'){
+    $.getJSON('http://apis.is/flight?language=en&type=arrivals').then(function(serverRes){
+			console.log(serverRes)
+     })
+
+     $.getJSON('http://apis.is/flight?language=en&type=departures').then(function(serverRes){
+			console.log(serverRes)
+    })
+    return
+  }
+
+}
